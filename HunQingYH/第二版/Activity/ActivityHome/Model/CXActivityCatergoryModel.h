42643ace751cc10dab42857b71f1a355344470fa
can <2212229342@qq.com>
2019-12-28 16:14:52 +0800

    免费领

---
+++ HunQingYH/第二版/Activity/ActivityHome/Model/CXActivityCatergoryModel.h
@@ -0,0 +1,20 @@
+//
+//  CXActivityCatergoryModel.h
+//  HunQingYH
+//
+//  Created by canxue on 2019/12/27.
+//  Copyright © 2019 YanpengLee. All rights reserved.
+//
+// - - - - - - - - - - - - - - 免费领 分类列表 - - - - - - - - - - - - - - - - - - - - - -
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CXActivityCatergoryModel : NSObject
+
+@property (nonatomic, copy) NSString *Id;    // 这里是个注释哦～
+@property (nonatomic, copy) NSString *Name;    // name
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HunQingYH/第二版/Activity/ActivityHome/Model/CXActivityCatergoryModel.m
@@ -0,0 +1,13 @@
+//
+//  CXActivityCatergoryModel.m
+//  HunQingYH
+//
+//  Created by canxue on 2019/12/27.
+//  Copyright © 2019 YanpengLee. All rights reserved.
+//
+
+#import "CXActivityCatergoryModel.h"
+
+@implementation CXActivityCatergoryModel
+
+@end
